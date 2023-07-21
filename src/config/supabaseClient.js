3be41cca4ccf5b47e330d5f2c2c1da0261b11940
polobryn <polobryn@yahoo.co.uk>
2023-07-22 00:12:29 +0100

    env variables

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
-const supabaseKey = process.env.REACT_APP_ANON_KEY
+const supabaseUrl = https://stujybrsreksohyiqtdu.supabase.co
+const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0dWp5YnJzcmVrc29oeWlxdGR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk5NTY0OTksImV4cCI6MjAwNTUzMjQ5OX0.slvxjc2x-JQyaDNcjAnXlL49slHiXHU8PdXvPtEs964
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
